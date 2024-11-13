c414cde23251fd3ac77ca2387789f6c510a42126
greg <greggoralski@gmail.com>
2024-11-13 16:43:13 -0500

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('first-button').addEventListener('click', function() {
+        document.getElementById('chair-content').innerHTML = 'This chair has awesome material';
+});
+
+const modelViewer = document.getElementById('avona');
+
+
+// example of changing model colour
+// document.getElementById("change-colour").addEventListener("click", function() {
+//     document.getElementById("model").src = "/models/teal.glb";
+// });
